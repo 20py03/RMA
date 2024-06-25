08701ee93f725c1539b491977a28fbaac53bc86f
20py03 <jovana.papric2@gmail.com>
2024-06-25 10:17:16 +0200

    Add space

--- app/src/main/java/hr/ferit/zavrsni/screens/ProfileScreen.kt
+++ app/src/main/java/hr/ferit/zavrsni/screens/ProfileScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
@@ -113,6 +114,7 @@ fun ProfileScreen(
                 ){
                     Text(text = "Edit", fontSize = 18.sp, fontWeight = FontWeight.Bold, color = DarkGray)
                 }
+                Spacer(modifier = Modifier.width(8.dp))
             }
         Row {
             ButtonComponent(
